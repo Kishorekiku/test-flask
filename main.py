b9f35e6e46a6883b8cc6b9b83f5615fd088ccd3f
Kishorekiku <118597695+Kishorekiku@users.noreply.github.com>
2024-02-05 12:46:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import requests
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-  return render_template('index.html')
+def home():
+    return render_template('home.html')
 
-if __name__ == '__main__':
-  app.run(port=5000)
+@app.route('/helpline')
+def help():
+    return render_template('help.html')
+
+@app.route('/track', methods=['POST'])
+def track():
+    if request.method == 'POST':
+        url = request.form['user_url']
+        resp = requests.get(url)
+        redirect_ulr = resp.history
+        return render_template('track.html', endurl = resp, redirect = redirect_ulr)
+
+if __name__=='__main__':
+    app.run(debug=True)
+
